46a4b7412729c7660932e5728f36aac7b8e0d1dc
Dmitry Anisimov <danston@ymail.com>
2019-07-01 18:01:10 +0200

    updated install

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir /usr/local/bin/cgal-examples/
-cp -r Shape_detection /usr/local/bin/cgal-examples/Shape_detection
+cp -r Shape_detection /Users/monet/Documents/brew/test/Shape_detection
