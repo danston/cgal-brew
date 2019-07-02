8c10123006e6ba981bcf2fd8a078d35b98045b5b
Dmitry Anisimov <danston@ymail.com>
2019-07-02 16:17:04 +0200

    fix

--- make-package.sh
+++ make-package.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-tar --exclude='./.git' --exclude='./make-package.sh' --exclude='./README.md' -zcvf "cgal-examples-4.14.tar.gz" .
+tar czf cgal-examples-4.14.tar.gz Shape_detection
 shasum -a 256 /Users/monet/Documents/brew/cgal-examples/cgal-examples-4.14.tar.gz
